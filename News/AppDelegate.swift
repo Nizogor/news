c72206bd0b82ee8640966d30dbe3a9526f45d6be
Nikita Teplyakov <>
2020-05-15 23:30:39 +0300

    add new project

---
+++ News/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  News
+//
+//  Created by Nikita Teplyakov on 15.05.2020.
+//  Copyright © 2020 Nikita Teplyakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = UIViewController()
+		window?.makeKeyAndVisible()
+
+		return true
+	}
+}
+

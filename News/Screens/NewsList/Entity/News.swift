b5e16203e7e38e86d5bed081675604a54d16fb80
Nikita Teplyakov <>
2020-05-19 00:18:26 +0300

    add news domain model

---
+++ News/Screens/NewsList/Entity/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  News
+//
+//  Created by Nikita Teplyakov on 18.05.2020.
+//  Copyright © 2020 Nikita Teplyakov. All rights reserved.
+//
+
+struct News {
+	let source: String
+	let link: String
+	let date: String
+	let title: String
+	let imageLink: String?
+	let description: String
+}
